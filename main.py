a8ea7482e3402800d91f30db4773e4afac5cd35a
cmmille <cmmiller.bio@gmail.com>
2022-03-29 22:52:49 -0600

    Added json support. Added search functionality.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 from password_generator import gen_password
 import ctypes
 import pyperclip
+import json
 
 FONT = ('Arial', 16)
 EMAIL = "youremail@email.com"
@@ -29,23 +30,51 @@ def save_password():
     new_site = web_input.get()
     new_user = email_input.get()
     new_pass = pass_input.get()
+    new_data = {
+        new_site: {
+            "email": new_user,
+            "password": new_pass
+        }
+    }
 
     # Check for null input
     if len(new_site) == 0 or len(new_user) == 0 or len(new_pass) == 0:
         messagebox.showinfo(title="Error", message="Missing information, please ensure all fields are filled.")
     else:
-        # Confirm details
-        confirm_msg = f"These are the details entered:\nEmail: {new_user}\nPassword: {new_pass}\nSave password?"
-        is_ok = messagebox.askokcancel(title=new_site, message=confirm_msg)
-
-        if is_ok:
-            # Write inputs to file
-            with open("./passwords.txt", "a") as file:
-                file.write(f"{new_site}\t{new_user}\t{new_pass}\n")
+        try:
+            # Read old data
+            with open("./passwords.json", "r") as file:
+                data = json.load(file)
+                # Update data
+                data.update(new_data)
+        except FileNotFoundError:
+            data = new_data
+        finally:
+            with open("./passwords.json", "w") as file:
+                # Write data to file
+                json.dump(data, file, indent=4)
+                messagebox.showinfo("Success", f"Successfully saved password for {new_site} ")
 
             # Clear inputs
             web_input.delete(0, END)
             pass_input.delete(0, END)
+
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def search_passwords():
+    query = web_input.get()
+
+    try:
+        with open("./passwords.json", "r") as file:
+            data = json.load(file)
+            found_email = data[query]["email"]
+            found_password = data[query]["password"]
+    except FileNotFoundError:
+        messagebox.showinfo("Error", "No saved passwords!\nTry adding some passwords first.")
+    except KeyError:
+        messagebox.showinfo("Error", f"No accounts found for {query}")
+    else:
+        messagebox.showinfo(query, f"User: {found_email}\nPassword: {found_password}")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -70,7 +99,7 @@ pass_lbl = Label(text="Password: ", font=FONT)
 pass_lbl.grid(row=3, column=0, sticky="e")
 
 # ---Inputs --- #
-web_input = Entry(width=40, font=FONT)
+web_input = Entry(width=22, font=FONT)
 web_input.grid(row=1, column=1, columnspan=2, sticky="w")
 web_input.focus()
 
@@ -82,6 +111,9 @@ pass_input = Entry(width=22, font=FONT)
 pass_input.grid(row=3, column=1, sticky="w")
 
 # --- Buttons --- #
+search_btn = Button(text="Search", width=32, command = search_passwords)
+search_btn.grid(row=1, column=2, sticky="e")
+
 gen_btn = Button(text="Generate Password", width=32, command=set_random_password)
 gen_btn.grid(row=3, column=2, sticky="e")
 
